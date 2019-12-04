71a1f96503a14cbf7e1c7727049cafbf038cf356
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 04:15:51 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,4 +14,17 @@ class Author
     end
     authors_posts
   end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def post_count
+
+  end
 end

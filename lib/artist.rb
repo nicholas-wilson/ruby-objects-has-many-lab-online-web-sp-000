2d0cbb82c9272fbb70f671c56bdad51a2d4206de
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 03:49:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   end
 
   def songs
-    Song.all.collect { |song| song.artist == self}
+    artist_songs = Song.all.collect { |song| song.artist == self}
   end
 
   def add_song(song)
